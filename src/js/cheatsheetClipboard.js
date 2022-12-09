9c062b9bf0ccfc8f513e626845b103eff9773912
DADAKIM <d.ya96v@gmail.com>
2022-12-09 20:22:58 +0900

    add: 클립보드 기능 구현 - 치트시트 기능 구현

--- src/js/cheatsheetClipboard.js
+++ src/js/cheatsheetClipboard.js
@@ -1,2 +1,13 @@
 const $cheatStatementBtn = document.querySelectorAll(".cheatsheet-clipboard-btn");
 
+$cheatStatementBtn.forEach(button => {
+  button.addEventListener("click", () => {
+    if(button.nextElementSibling.classList.contains("cheatsheet-sql")){
+      window.navigator.clipboard
+      .writeText(button.nextElementSibling.innerText)
+      .then(() => {
+        alert("복사완료");
+      });
+    };
+  });
+});
