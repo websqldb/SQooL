9fb79bd013de67bcdf1d1a5ffe9956b4dcdf5d71
sihyeon lee <hey210056@gmail.com>
2022-12-09 19:20:24 +0900

    add: 클립보드 기능 구현 - 메인페이지 변수 선언

---
+++ src/js/clipboard.js
@@ -0,0 +1,4 @@
+const $mainStatementClipboard = document.querySelector(".code");
+const $mainStatementBtn = $mainStatementClipboard.querySelector(".btn-run");
+const $mainStatementCodeLine =
+  $mainStatementClipboard.querySelector(".code-line");
