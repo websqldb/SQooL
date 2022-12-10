b428e53a464934f7aea8eae5ff9f0efa7b739082
dev-yun <yunpro2766@gmail.com>
2022-12-10 16:52:18 +0900

    Feat: 메인의 사이드 sql문 선택 박스 클릭 시 존재하는 모든 sql syntax를 출력하도록 구현 todo: option 선택 시 해당 option의 base sql 구문이 출력되도록 구현해야함

--- src/js/sqlSideButton.js
+++ src/js/sqlSideButton.js
@@ -15,3 +15,22 @@ moreBtn.addEventListener('click', () => {
     });
   }
 });
+
+// 메인 sql select버튼 로직
+const selectSql = document.querySelector('#sqlSelect');
+const allSqlBtn = document.querySelectorAll('.sql-btn');
+const domFrag = new DocumentFragment();
+const sqlSyntaxArray = [];
+
+allSqlBtn.forEach((sqlBtn) => {
+  sqlSyntaxArray.push(sqlBtn.innerText.replaceAll(' ', '_'));
+});
+
+sqlSyntaxArray.forEach((sqlSyntax) => {
+  const sqlOption = document.createElement('option');
+  sqlOption.value = sqlSyntax;
+  sqlOption.innerText = sqlSyntax;
+  domFrag.appendChild(sqlOption);
+});
+
+selectSql.appendChild(domFrag);
