53f985a330dd322c865c3a8f2a04a106fd4017b9
jihyunglee <jhlee7125@gmail.com>
2022-12-12 00:44:36 +0900

    feat: more 버튼 클릭 시 select 요소 visibility 스타일을 visible로 변경

--- src/js/sqlSideButton.js
+++ src/js/sqlSideButton.js
@@ -1,26 +1,13 @@
 // 메인 sql more 버튼 클릭 로직
 const moreBtn = document.querySelector(".more-btn");
-const selectOpt = document.querySelector("#sqlSelect");
+const selectSql = document.querySelector("#sqlSelect");
 const sqlBtnMore = document.querySelectorAll(".sql-btn-more");
 
 moreBtn.addEventListener("click", () => {
-  const isHidden = sqlBtnMore[0].className.includes("sr-only");
-
-  if (isHidden) {
-    sqlBtnMore.forEach((sqlBtn) => {
-      sqlBtn.classList.remove("sr-only");
-    });
-  } else {
-    sqlBtnMore.forEach((sqlBtn) => {
-      sqlBtn.classList.add("sr-only");
-    });
-  }
-
-  selectOpt.style.visibility = "visible";
+  selectSql.style.visibility = "visible";
 });
 
 // 메인 sql select버튼 로직
-const selectSql = document.querySelector("#sqlSelect");
 const allSqlBtn = document.querySelectorAll(".sql-btn");
 const domFrag = new DocumentFragment();
 const sqlSyntaxArray = [];
