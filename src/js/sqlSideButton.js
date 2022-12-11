e5bec62c58e8a42cf8335546b4680e18896758fd
jihyunglee <jhlee7125@gmail.com>
2022-12-11 22:33:05 +0900

    feat: more 버튼 클릭시 select box의 visibility 속성을 hidden에서 visible로 변경

--- src/js/sqlSideButton.js
+++ src/js/sqlSideButton.js
@@ -1,33 +1,36 @@
 // 메인 sql more 버튼 클릭 로직
-const moreBtn = document.querySelector('.more-btn');
-const sqlBtnMore = document.querySelectorAll('.sql-btn-more');
+const moreBtn = document.querySelector(".more-btn");
+const selectOpt = document.querySelector("#sqlSelect");
+const sqlBtnMore = document.querySelectorAll(".sql-btn-more");
 
-moreBtn.addEventListener('click', () => {
-  const isHidden = sqlBtnMore[0].className.includes('sr-only');
+moreBtn.addEventListener("click", () => {
+  const isHidden = sqlBtnMore[0].className.includes("sr-only");
 
   if (isHidden) {
     sqlBtnMore.forEach((sqlBtn) => {
-      sqlBtn.classList.remove('sr-only');
+      sqlBtn.classList.remove("sr-only");
     });
   } else {
     sqlBtnMore.forEach((sqlBtn) => {
-      sqlBtn.classList.add('sr-only');
+      sqlBtn.classList.add("sr-only");
     });
   }
+
+  selectOpt.style.visibility = "visible";
 });
 
 // 메인 sql select버튼 로직
-const selectSql = document.querySelector('#sqlSelect');
-const allSqlBtn = document.querySelectorAll('.sql-btn');
+const selectSql = document.querySelector("#sqlSelect");
+const allSqlBtn = document.querySelectorAll(".sql-btn");
 const domFrag = new DocumentFragment();
 const sqlSyntaxArray = [];
 
 allSqlBtn.forEach((sqlBtn) => {
-  sqlSyntaxArray.push(sqlBtn.innerText.replaceAll(' ', '_'));
+  sqlSyntaxArray.push(sqlBtn.innerText.replaceAll(" ", "_"));
 });
 
 sqlSyntaxArray.forEach((sqlSyntax) => {
-  const sqlOption = document.createElement('option');
+  const sqlOption = document.createElement("option");
   sqlOption.value = sqlSyntax;
   sqlOption.innerText = sqlSyntax;
   domFrag.appendChild(sqlOption);
