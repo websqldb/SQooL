42978d4876bb03c077fff48c7cc6e1c40be72e80
wang-yurin <king_1994@naver.com>
2022-12-14 23:25:57 +0900

    Fix: 자동완성 키 Enter에서 Tab으로 변경 - Enter key로 자동완성 후 다시 Enter key 누르면 개행 되지 않는 부분 Tab key로 자동완성 되도록 수정

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,22 +1,24 @@
 var AUTOCOMPLETE_TABLES = {
-  'hello': ['world', 'neighbor', 'there'],
-  'reply': ['rumble', 'hellodley', 'hi_there']
-  }
-  window.onload = function() {
+  hello: ['world', 'neighbor', 'there'],
+  reply: ['rumble', 'hellodley', 'hi_there'],
+};
+window.onload = function () {
+  window.editor = CodeMirror.fromTextArea(
+    document.getElementById('codeeditor'),
+    {
+      mode: 'text/x-sql',
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      matchBrackets: true,
+      autofocus: true,
+      extraKeys: { Tab: 'autocomplete' },
+    }
+  );
 
-  window.editor = CodeMirror.fromTextArea(document.getElementById('codeeditor'), {
-    mode: 'text/x-sql',
-    indentWithTabs: true,
-    smartIndent: true,
-    lineNumbers: true,
-    matchBrackets : true,
-    autofocus: true,
-    extraKeys: {"Enter": "autocomplete"}
-  });
-
-  CodeMirror.commands.autocomplete = function(cm) {
+  CodeMirror.commands.autocomplete = function (cm) {
     CodeMirror.showHint(cm, CodeMirror.hint.sql, {
-        tables: AUTOCOMPLETE_TABLES
+      tables: AUTOCOMPLETE_TABLES,
     });
-  }
   };
+};
