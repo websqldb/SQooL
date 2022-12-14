308d608966c89d322b9e40bb64321e05d400672e
youngjun.0427 <dlfgudwns12@gmail.com>
2022-12-15 01:13:20 +0900

    feat:side bar 버튼 클릭시 main result width 조정 기능 추가

--- src/js/mainSideBar.js
+++ src/js/mainSideBar.js
@@ -4,10 +4,13 @@ const $main = document.querySelector("main");
 function toggleSideBar() {
   const $sideBar = document.querySelector(".side-bar");
   const $resultTable = $main.querySelector(".sec-result table");
+  //추가코드
+  const $resultSection = $main.querySelector(".sec-result");
 
   $sideBar.classList.toggle("foldActive");
   $main.classList.toggle("foldActive-wrap");
   $resultTable.classList.toggle("foldActive-wrap");
+  $resultSection.classList.toggle("stretch");
   $foldBtn.classList.toggle("fold-btn-toggle");
 }
 
