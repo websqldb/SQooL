2413cc96d4616b9ff97b190a44d494fa0f0fb5c7
sihyeon lee <hey210056@gmail.com>
2022-12-15 15:39:55 +0900

    add: toast 스타일링 구현

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -2,8 +2,10 @@ const $mainStatementClipboard = document.querySelector(".code");
 const $mainStatementBtn = document.querySelector(".btn-copy");
 const $mainStatementCodeLine =
   $mainStatementClipboard.querySelector("textarea");
+const $toastClipBoard = document.querySelector("#toastClipBoard");
 
-$mainStatementBtn.addEventListener("click", () => {
+$mainStatementBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   window.navigator.clipboard
     .writeText($mainStatementCodeLine.value)
     .then(() => {
