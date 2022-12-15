69fb6121c8061588d9818de5927d9db80960e36f
Seulki Yun <icstuckyi@gmail.com>
2022-12-15 22:08:42 +0900

    feat: 코드미러 키워드 자동완성 기능 개선

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,36 +1,52 @@
 var AUTOCOMPLETE_TABLES = {
-  hello: ['world', 'neighbor', 'there'],
-  reply: ['rumble', 'hellodley', 'hi_there'],
+  hello: ["world", "neighbor", "there"],
+  reply: ["rumble", "hellodley", "hi_there"],
 };
-let defaultValue = 'SELECT * FROM Student';
+let defaultValue = "SELECT * FROM Student";
 
 window.onload = function () {
   window.editor = CodeMirror.fromTextArea(
-    document.getElementById('codeeditor'),
+    document.getElementById("codeeditor"),
     {
-      mode: 'text/x-sql',
+      mode: "text/x-sql",
       indentWithTabs: true,
       smartIndent: true,
       lineNumbers: true,
       matchBrackets: true,
       autofocus: true,
-      extraKeys: { Tab: 'autocomplete' },
+      extraKeys: { Tab: "autocomplete" },
     }
   );
-window.editor.setValue(defaultValue);
-/*
+
+  window.editor.on("keydown", function (cm, event) {
+    // a ~ z
+    // 1 ~ F12
+    // delete ~ 9
+    if (
+      (!event.ctrlKey && event.keyCode >= 65 && event.keyCode <= 90) ||
+      (event.keyCode >= 97 && event.keyCode <= 122) ||
+      (event.keyCode >= 46 && event.keyCode <= 57)
+    ) {
+      CodeMirror.commands.autocomplete(cm, null, { completeSingle: false });
+    }
+  });
+
+  window.editor.setValue(defaultValue);
+  /*
   CodeMirror.commands.autocomplete = function (cm) {
     CodeMirror.showHint(cm, CodeMirror.hint.sql, {
       tables: AUTOCOMPLETE_TABLES,
     });
   };
 */
-// 탭 누르지 않고 바로 실행되도록 설정
+  // 탭 누르지 않고 바로 실행되도록 설정
   window.editor.on("keyup", function (cm, event) {
-    if (!cm.state.completionActive && 
-        event.keyCode != 13 && event.keyCode != 48 ) {        
-        CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
+    if (
+      !cm.state.completionActive &&
+      event.keyCode != 13 &&
+      event.keyCode != 48
+    ) {
+      CodeMirror.commands.autocomplete(cm, null, { completeSingle: false });
     }
-});
-
+  });
 };
