04410d083e745ca08b8a48d5c536a3a889578545
DADAKIM <d.ya96v@gmail.com>
2022-12-15 15:05:02 +0900

    fix: 복사후 새로고침 문제 해결

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -3,7 +3,8 @@ const $mainStatementBtn = document.querySelector(".btn-copy");
 const $mainStatementCodeLine =
   $mainStatementClipboard.querySelector("textarea");
 
-$mainStatementBtn.addEventListener("click", () => {
+$mainStatementBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   window.navigator.clipboard
     .writeText($mainStatementCodeLine.value)
     .then(() => {
