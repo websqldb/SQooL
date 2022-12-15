bcb5ca650eacd37992dc09282bac58b4174ad859
sihyeon lee <hey210056@gmail.com>
2022-12-15 15:57:39 +0900

    add: clipboard 토스트 구현

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -2,13 +2,25 @@ const $mainStatementClipboard = document.querySelector(".code");
 const $mainStatementBtn = document.querySelector(".btn-copy");
 const $mainStatementCodeLine =
   $mainStatementClipboard.querySelector("textarea");
-const $toastClipBoard = document.querySelector("#toastClipBoard");
+
+function toast() {
+  const $toastClipBoard = document.querySelector("#toastClipBoard");
+  $toastClipBoard.classList.contains("reveal")
+    ? (clearTimeout(removeToast),
+      (removeToast = setTimeout(function () {
+        $toastClipBoard.classList.remove("reveal");
+      }, 1000)))
+    : (removeToast = setTimeout(function () {
+        $toastClipBoard.classList.remove("reveal");
+      }, 1000));
+  $toastClipBoard.classList.add("reveal");
+}
 
 $mainStatementBtn.addEventListener("click", (e) => {
   e.preventDefault();
   window.navigator.clipboard
     .writeText($mainStatementCodeLine.value)
     .then(() => {
-      alert("복사완료");
+      toast();
     });
 });
