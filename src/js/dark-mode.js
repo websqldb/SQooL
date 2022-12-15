7c0d9dc7683818441143968130c5d87f46ba1eae
khv2644511 <khv2644511@naver.com>
2022-12-16 00:46:52 +0900

    Cheatsheet 다크 모드

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -3,15 +3,18 @@ const $switchBtn = $body.querySelector(".btn-darkmode");
 
 function toggleMode() {
   const $sideBar = $body.querySelector(".side-bar");
-  const $header = $body.querySelector(".global-header")
+  const $header = $body.querySelector(".global-header");
   const $main = $body.querySelector("main");
+  const $footer = $body.querySelector(".sql-footer");
   const $sqlBtnWrap = $sideBar.querySelector(".sql-btn-wrap");
+  // sql-btn-wrap sideBar=> body로 수정 가넝?
 
   $body.classList.toggle("dark-mode");
   $sideBar.classList.toggle("dark-mode");
   $header.classList.toggle("dark-mode");
   $switchBtn.classList.toggle("dark-mode");
   $main.classList.toggle("dark-mode");
+  $footer.classList.toggle("dark-mode");
   $sqlBtnWrap.classList.toggle("dark-mode");
 }
 
