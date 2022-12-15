9ff283f706d1d5f4396bbb15af17918ccfad6de9
DADAKIM <d.ya96v@gmail.com>
2022-12-15 15:59:26 +0900

    add: cheatsheet clipboard 토스트 구현

--- src/js/cheatsheetClipboard.js
+++ src/js/cheatsheetClipboard.js
@@ -1,13 +1,27 @@
 const $cheatStatementBtn = document.querySelectorAll(".cheatsheet-clipboard-btn");
 
+function toast() {
+  const $csToastClipBoard = document.querySelector("#toastClipBoard");
+
+  $csToastClipBoard.classList.contains("reveal") ?
+      (clearTimeout(removeToast), removeToast = setTimeout(function () {
+        $csToastClipBoard.classList.remove("reveal")
+      }, 1000)) :
+      removeToast = setTimeout(function () {
+        $csToastClipBoard.classList.remove("reveal")
+      }, 1000)
+      $csToastClipBoard.classList.add("reveal")
+}
+
 $cheatStatementBtn.forEach(button => {
   button.addEventListener("click", () => {
     if(button.nextElementSibling.classList.contains("cheatsheet-sql")){
       window.navigator.clipboard
       .writeText(button.nextElementSibling.innerText)
       .then(() => {
-        alert("복사완료");
+        toast();
       });
     };
   });
 });
+
