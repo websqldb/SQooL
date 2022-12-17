43d40617c2f68a7d341dd8fab4eb7e4548bf9eaa
Soohyun Jung <sasha1107@naver.com>
2022-12-18 00:50:39 +0900

    Fix: 기본 구문 버튼 기능 수정
    - 버튼 클릭 시 기본 구문이 에디터에 추가되는 기능 동작하도록 수정
    - 에디터 구현 방식 변경으로 기존 코드가 작동하지 않는 문제 해
    결

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,3 +1,195 @@
+// 등록된 모든 사이드 sql 버튼에 대한 sql 기본 입력 구문 작성(sql 구문이 고정되면 추가 예정, 현재 기능만 )
+let statement = "";
+const addStatement = (sqlSyntax) => {
+  // let statement = "";
+  switch (sqlSyntax) {
+    case "SELECT":
+      statement = `SELECT * FROM table_name;`;
+      break;
+    case "GROUP_BY":
+      statement = `SELECT column_name(s)
+FROM table_name
+GROUP BY column_name(s)`;
+      break;
+    case "ORDER_BY":
+      statement = `SELECT * FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;;
+      `;
+      break;
+    case "UPDATE":
+      statement = `UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;`;
+      break;
+    case "INSERT_INTO":
+      statement = `INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);`;
+      break;
+    case "WHERE":
+      statement = `SELECT * FROM table_name
+WHERE condition;`;
+      break;
+    case "DELETE":
+      statement = `DELETE FROM table_name WHERE condition;`;
+      break;
+    case "INNER_JOIN":
+      statement = `SELECT column_name(s) FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;`;
+      break;
+    case "INSERT_INTO_SELECT":
+      statement = `INSERT INTO table2
+SELECT * FROM table1
+WHERE condition;`;
+      break;
+    case "LIMIT":
+      statement = `SELECT * FROM table_name
+LIMIT rows;`;
+      break;
+    case "AND":
+      statement = `SELECT column1, column2
+FROM table_name
+WHERE condition1 AND condition2 AND condition3;`;
+      break;
+    case "OR":
+      statement = `SELECT column1, column2
+FROM table_name
+WHERE condition1 OR condition2 OR condition3;`;
+      break;
+    case "NOT":
+      statement = `SELECT column1, column2
+FROM table_name
+WHERE NOT condition;`;
+      break;
+    case "IS_NULL":
+      statement = `SELECT column_names
+FROM table_name
+WHERE column_name IS NULL;`;
+      break;
+    case "IS_NOT_NULL":
+      statement = `SELECT column_names
+FROM table_name
+WHERE column_name IS NOT NULL;`;
+      break;
+    case "IN":
+      statement = `SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);`;
+      break;
+    case "BETWEEN":
+      statement = `SELECT column_name(s)
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;`;
+      break;
+    case "COUNT":
+      statement = `SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "SUM":
+      statement = `SELECT SUM(column_name)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "MIN":
+      statement = `SELECT MIN(column_name)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "MAX":
+      statement = `SELECT MAX(column_name)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "AVG":
+      statement = `SELECT AVG(column_name)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "AS":
+      statement = `SELECT column_name AS alias_name
+FROM table_name;`;
+      break;
+    case "HAVING":
+      statement = `SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+HAVING condition
+ORDER BY column_name(s);`;
+      break;
+    case "CASE":
+      statement = `CASE
+  WHEN condition1 THEN result1
+  WHEN condition2 THEN result2
+  WHEN condition3 THEN resultN
+  ELSE result
+END;`;
+      break;
+    case "DISTINCT":
+      statement = `SELECT DISTINCT columnn1, column2, ...
+FROM table_name;`;
+      break;
+    case "LEFT_JOIN":
+      statement = `SELECT column_name(s)
+FROM table1
+LEFT JOIN table2
+ON table1.column_name = table2.column_name;`;
+      break;
+    case "RIGHT_JOIN":
+      statement = `SELECT column_name(s)
+FROM table1
+RIGTH JOIN table2
+ON table1.column_name = table2.column_name;`;
+      break;
+    case "FULL_OUTER_JOIN":
+      statement = `SELECT column_name(s)
+FROM table1
+FULL OUTER JOIN table2
+ON table1.column_name = table2.column_name
+WHERE condition;`;
+      break;
+    case "UNION":
+      statement = `SELECT column_name(s) FROM table1
+UNION 
+SELECT column_name(s) FROM table2;`;
+      break;
+    case "UNION_ALL":
+      statement = `SELECT column_name(s) FROM table1
+UNION ALL
+SELECT column_name(s) FROM table2;`;
+      break;
+    case "EXISTS":
+      statement = `SELECT column_name(s)
+FROM table_name
+WHERE EXISTS
+(SELECT column_name FROM table_name WHERE condition);`;
+      break;
+    case "ANY":
+      statement = `SELECT column_name(s)
+FROM table_name
+WHERE column_name operatoor ANY
+  (SELECT column_name
+  FROM table_name
+  WHERE condition);`;
+      break;
+    case "ALL":
+      statement = `SELECT ALL column_name(s)
+FROM table_name
+WHERE condition;`;
+      break;
+    case "SELECT_INTO":
+      statement = `SELECT column1 INTO column2 
+FROM table_name 
+WHERE condition;`;
+      break;
+    default:
+      statement;
+  }
+  window.editor.setValue(statement);
+  // codeLine.innerHTML = statement;
+};
+
 var AUTOCOMPLETE_TABLES = {
   department: ['학과번호', '학과이름', '학과담당자', '학과전화번호'],
   grade: ['학번', '학기', '평균학점'],
@@ -34,9 +226,8 @@ var AUTOCOMPLETE_TABLES = {
     '학점',
   ],
 };
-let defaultValue = 'SELECT * FROM Student';
 
-window.onload = function () {
+window.onload = function codemirroreditor() {
   window.editor = CodeMirror.fromTextArea(
     document.getElementById('codeeditor'),
     {
@@ -49,7 +240,7 @@ window.onload = function () {
       extraKeys: { Tab: 'autocomplete' },
     }
   );
-  window.editor.setValue(defaultValue);
+  window.editor.setValue('SELECT * FROM Student');
 
   CodeMirror.commands.autocomplete = function (cm) {
     CodeMirror.showHint(cm, CodeMirror.hint.sql, {
@@ -78,8 +269,10 @@ const $mainStatementBtn = document.querySelector(".btn-copy");
 
 $mainStatementBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const $mainStatementCodeLine = document.querySelector(".CodeMirror-line");
-  const codeTxt = $mainStatementCodeLine.textContent;
+  // const $mainStatementCodeLine = document.querySelector(".CodeMirror-line");
+  const $mainStatementCodeLine = document.querySelectorAll(".CodeMirror-line");
+  let codeTxt = ""
+  $mainStatementCodeLine.forEach(i => codeTxt += i.textContent + "\n")
   window.navigator.clipboard
     .writeText(codeTxt)
     .then(() => {
@@ -102,3 +295,18 @@ function toast() {
       }, 1000));
   $toastClipBoard.classList.add("reveal");
 }
+
+const sqlBtnItems = document.querySelector(".sql-btn-item");
+
+// 사이드 select option 선택 시 sql statement에 기본 구문 출력
+sqlSelect.addEventListener("change", (e) => {
+  addStatement(e.target.value);
+});
+
+// 사이드 버튼 클릭 시 sql statement에 기본 구문 출력
+sqlBtnItems.addEventListener("click", (e) => {
+  if (e.target.classList.contains("sql-btn")) {
+    const sqlBtn = e.target;
+    addStatement(sqlBtn.dataset.sql);
+  }
+});
