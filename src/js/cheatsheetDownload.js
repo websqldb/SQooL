fb98a3ef060fae4d201b68cc85a45cce769b777f
Ocknyer <kdu4045@naver.com>
2022-12-18 23:01:57 +0900

    fix : 다운로드 버튼 클릭이벤트 연결

--- src/js/cheatsheetDownload.js
+++ src/js/cheatsheetDownload.js
@@ -1,23 +1,29 @@
 const downloadBtn = document.querySelector('.cheatsheet-download');
 
-const element = document.createElement('a');
-const makeA = () => {
-  element.setAttribute('href', '../../data/grade.json');
-  element.setAttribute('download', 'test');
-  element.style.width = '15px';
-  element.style.height = '15px';
-  element.style.position = 'absoulte';
-  element.style.left = '0';
-  element.style.top = '0';
-  element.style.display = 'block';
-  downloadBtn.appendChild(element);
+// const createDownloadTag = () => {
+//   const element = document.createElement('a');
+//   element.setAttribute('href', '../../data/grade.json');
+//   element.setAttribute('download', 'test');
+//   element.style.width = '15px';
+//   element.style.height = '15px';
+//   element.style.position = 'absoulte';
+//   element.style.left = '0';
+//   element.style.top = '0';
+//   element.style.display = 'block';
+//   downloadBtn.appendChild(element);
+// };
+
+// createDownloadTag();
+
+const downlodePdf = (filename, filepath) => {
+  const element = document.createElement("a");
+  element.setAttribute("href", "../../data/grade.json");
+  element.setAttribute("download", "test");
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 };
 
-makeA();
-
-downloadBtn.addEventListener('click', (e) => {
-  // e.preventDefault();
-  // element.click();
-  console.log('click');
-  // downloadBtn.removeChild(element);
+downloadBtn.addEventListener("click", (e) => {
+  downlodePdf();
 });
