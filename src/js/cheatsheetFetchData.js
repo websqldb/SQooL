f21bdf3a61e84e7a2673939f368957358d62ac4e
Soohyun Jung <sasha1107@naver.com>
2022-12-19 21:48:55 +0900

    style: 치트시트 코드 하이라이팅 구현

--- src/js/cheatsheetFetchData.js
+++ src/js/cheatsheetFetchData.js
@@ -9,6 +9,9 @@ const fileNameArray = [
   "manageTable",
 ];
 
+let sqlKeyWords = "abort action add after all analyze attach autoincrement before begin cascade case cast check collate column commit conflict constraint cross current_date current_time current_timestamp database default deferrable deferred detach each else end escape except exclusive exists explain fail for foreign full glob if ignore immediate index indexed initially inner instead intersect isnull key left limit match natural no notnull null of offset outer plan pragma primary query raise recursive references regexp reindex release rename replace restrict right rollback row savepoint temp temporary then to transaction trigger unique using vacuum view virtual when with without alter and as asc between by count create delete desc distinct drop from group having in insert into is join like not on or order select set table union update values where limit desc asc --right --left --full --where --select desc; asc;".split(" ").map(i => i.toUpperCase());
+sqlKeyWords.push("and");
+
 async function loadTotalData() {
   for (let i = 0; i < fileNameArray.length; i++) {
     const response = await fetch(
@@ -19,7 +22,7 @@ async function loadTotalData() {
         const section = document.createElement("section");
         const sectionName = document.createElement("h3");
         const sectionUl = document.createElement("ul");
-
+        
         section.classList.add("cheatsheet-wrapper");
         sectionUl.classList.add("cheatsheet-list");
         sectionName.innerHTML = data[0].title;
@@ -27,6 +30,7 @@ async function loadTotalData() {
         section.appendChild(sectionUl);
         for (let i = 1; i < data.length; i++) {
           const sectionLi = document.createElement("li");
+          let code = [];
           if (i === 1 && data[i].img) {
             sectionLi.innerHTML = `
             <img class="cheatsheet-sql" style="width: 100%" src=${data[1].img}>
@@ -38,10 +42,22 @@ async function loadTotalData() {
           } else {
             const stringSplit = data[i].syntax.split("\n");
 
-            const syntaxReplace = data[i]?.syntax.replace(
+            let syntaxReplace = data[i]?.syntax.replace(
               /(?:\r\n|\r|\n)/g,
               "<br>"
             );
+            syntaxReplace.split("<br>").forEach(line => {
+              line.split(" ").forEach(i => {
+                if (sqlKeyWords.includes(i)) {
+                  code += `<strong class="keyword">${i}</strong>` + ' '
+                }
+                else {
+                  code += i + ' ';
+                }   
+            })
+            code += '<br>'
+          });
+          
 
             sectionLi.innerHTML = `
               <button class="cheatsheet-clipboard-btn">
@@ -51,7 +67,7 @@ async function loadTotalData() {
                 />
               </button>
               <p class="cheatsheet-sql">
-                <strong class="cheatsheet-sql-statement">${syntaxReplace}</strong>
+                <strong class="cheatsheet-sql-statement">${code}</strong>
               </p>
               <p class="cheatsheet-sql-explanation">
                 ${data[i].desc}
