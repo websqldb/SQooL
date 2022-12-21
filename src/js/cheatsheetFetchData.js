a73dc5dc4b2eb04d29d78087dad3843b81f1ce70
DADAKIM <d.ya96v@gmail.com>
2022-12-21 15:14:20 +0900

    fix: 치트시트 클립보드기능 오류 수정

--- src/js/cheatsheetFetchData.js
+++ src/js/cheatsheetFetchData.js
@@ -95,6 +95,36 @@ async function loadTotalData() {
             });
           });
         });
+
+        // Cheatsheet Clipboard 복사기능 이벤트 추가
+        const $cheatStatementBtn = document.querySelectorAll(".cheatsheet-clipboard-btn");
+
+        function toast() {
+          const $csToastClipBoard = document.querySelector("#toastClipBoard");
+
+          $csToastClipBoard.classList.contains("reveal") ?
+              (clearTimeout(removeToast), removeToast = setTimeout(function () {
+                $csToastClipBoard.classList.remove("reveal")
+              }, 1000)) :
+              removeToast = setTimeout(function () {
+                $csToastClipBoard.classList.remove("reveal")
+              }, 1000)
+              $csToastClipBoard.classList.add("reveal")
+        }
+
+        $cheatStatementBtn.forEach(button => {
+          button.addEventListener("click", () => {
+            if(button.nextElementSibling.classList.contains("cheatsheet-sql")){
+              window.navigator.clipboard
+              .writeText(button.nextElementSibling.innerText)
+              .then(() => {
+                toast();
+              });
+            };
+          });
+        });
+
+
       });
   }
 }
