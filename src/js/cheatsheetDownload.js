7fbceae2cc54d581c77d9d55afdfef15de624743
youjin <yjinhann@gmail.com>
2022-12-22 22:55:04 +0900

    fix : 주석 삭제 및 pdf 파일 추가

--- src/js/cheatsheetDownload.js
+++ src/js/cheatsheetDownload.js
@@ -1,22 +1,5 @@
 const downloadBtn = document.querySelector('.cheatsheet-download');
 
-// const createDownloadTag = () => {
-//   const element = document.createElement('a');
-//   element.setAttribute('href', '../../data/grade.json');
-//   element.setAttribute('download', 'test');
-//   element.style.width = '15px';
-//   element.style.height = '15px';
-//   element.style.position = 'absoulte';
-//   element.style.left = '0';
-//   element.style.top = '0';
-//   element.style.display = 'block';
-//   downloadBtn.appendChild(element);
-// };
-
-// createDownloadTag();
-
-// test
-// pdf 파일 제작완료 후 파일경로 변경 예정
 const downlodePdf = (filename, filepath) => {
   const element = document.createElement('a');
   element.setAttribute('href', '../../data/grade.json');
