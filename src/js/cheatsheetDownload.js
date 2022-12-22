b41322ffe23b50e101cc239e6562d526a394edb8
Ocknyer <kdu4045@naver.com>
2022-12-22 22:57:16 +0900

    feat: pdf 다운로드 경로 추가

--- src/js/cheatsheetDownload.js
+++ src/js/cheatsheetDownload.js
@@ -1,14 +1,17 @@
-const downloadBtn = document.querySelector('.cheatsheet-download');
+const downloadBtn = document.querySelector(".cheatsheet-download");
 
 const downlodePdf = (filename, filepath) => {
-  const element = document.createElement('a');
-  element.setAttribute('href', '../../data/grade.json');
-  element.setAttribute('download', 'test');
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+    const element = document.createElement("a");
+    element.setAttribute(
+        "href",
+        "../../data/cheatsheetData/SQooL_CheatSheet.pdf"
+    );
+    element.setAttribute("download", "CheatSheet");
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 };
 
-downloadBtn.addEventListener('click', (e) => {
-  downlodePdf();
+downloadBtn.addEventListener("click", (e) => {
+    downlodePdf();
 });
