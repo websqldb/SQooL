1ad8bfbfa7d8a45af9dfb152da9020e1592448a7
yuzomi <im.the.best@kakao.com>
2022-12-27 12:49:18 +0900

    Fix: 치트시트 복사 아이콘 크기 수정

--- src/js/cheatsheetFetchData.js
+++ src/js/cheatsheetFetchData.js
@@ -1,36 +1,38 @@
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 const fileNameArray = [
-  'tableoutput',
-  'tablegetdata',
-  'getConditionData',
-  'getDataMultipleTable',
-  'function',
-  'grouping',
-  'manageTable',
+  "tableoutput",
+  "tablegetdata",
+  "getConditionData",
+  "getDataMultipleTable",
+  "function",
+  "grouping",
+  "manageTable",
 ];
 
 let sqlKeyWords =
-  'abort action add after all analyze attach autoincrement before begin cascade case cast check collate column commit conflict constraint cross current_date current_time current_timestamp database default deferrable deferred detach each else end escape except exclusive exists explain fail for foreign full glob if ignore immediate index indexed initially inner instead intersect isnull key left limit match natural no notnull null of offset outer plan pragma primary query raise recursive references regexp reindex release rename replace restrict right rollback row savepoint temp temporary then to transaction trigger unique using vacuum view virtual when with without alter and as asc between by count create delete desc distinct drop from group having in insert into is join like not on or order select set table union update values where limit desc asc --right --left --full --where --select desc; asc;'
-    .split(' ')
+  "abort action add after all analyze attach autoincrement before begin cascade case cast check collate column commit conflict constraint cross current_date current_time current_timestamp database default deferrable deferred detach each else end escape except exclusive exists explain fail for foreign full glob if ignore immediate index indexed initially inner instead intersect isnull key left limit match natural no notnull null of offset outer plan pragma primary query raise recursive references regexp reindex release rename replace restrict right rollback row savepoint temp temporary then to transaction trigger unique using vacuum view virtual when with without alter and as asc between by count create delete desc distinct drop from group having in insert into is join like not on or order select set table union update values where limit desc asc --right --left --full --where --select desc; asc;"
+    .split(" ")
     .map((i) => i.toUpperCase());
-sqlKeyWords.push('and');
+sqlKeyWords.push("and");
 
 async function loadTotalData() {
   for (let i = 0; i < fileNameArray.length; i++) {
-    const response = await fetch(`../../data/cheatsheetData/${fileNameArray[i]}.json`)
+    const response = await fetch(
+      `../../data/cheatsheetData/${fileNameArray[i]}.json`
+    )
       .then((res) => res.json())
       .then((data) => {
-        const section = document.createElement('section');
-        const sectionName = document.createElement('h3');
-        const sectionUl = document.createElement('ul');
+        const section = document.createElement("section");
+        const sectionName = document.createElement("h3");
+        const sectionUl = document.createElement("ul");
 
-        section.classList.add('cheatsheet-wrapper');
-        sectionUl.classList.add('cheatsheet-list');
+        section.classList.add("cheatsheet-wrapper");
+        sectionUl.classList.add("cheatsheet-list");
         sectionName.innerHTML = data[0].title;
         section.appendChild(sectionName);
         section.appendChild(sectionUl);
         for (let i = 1; i < data.length; i++) {
-          const sectionLi = document.createElement('li');
+          const sectionLi = document.createElement("li");
           let code = [];
           if (i === 1 && data[i].img) {
             sectionLi.innerHTML = `
@@ -41,30 +43,33 @@ async function loadTotalData() {
             </p>
             `;
           } else {
-            const stringSplit = data[i].syntax.split('\n');
+            const stringSplit = data[i].syntax.split("\n");
 
-            let syntaxReplace = data[i]?.syntax.replace(/(?:\r\n|\r|\n)/g, '<br>');
+            let syntaxReplace = data[i]?.syntax.replace(
+              /(?:\r\n|\r|\n)/g,
+              "<br>"
+            );
 
-            syntaxReplace.split('<br>').forEach((line) => {
-              line.split(' ').forEach((i) => {
+            syntaxReplace.split("<br>").forEach((line) => {
+              line.split(" ").forEach((i) => {
                 if (sqlKeyWords.includes(i)) {
-                  code += `<strong class="keyword">${i}</strong>` + ' ';
+                  code += `<strong class="keyword">${i}</strong>` + " ";
                 } else {
-                  code += i + ' ';
+                  code += i + " ";
                 }
               });
-              code += '<br>';
+              code += "<br>";
             });
 
-            if (data[i].desc.includes('\n')) {
-              data[i].desc = data[i].desc.replace('\n', '<br />');
+            if (data[i].desc.includes("\n")) {
+              data[i].desc = data[i].desc.replace("\n", "<br />");
             }
 
             sectionLi.innerHTML = `
               <button class="cheatsheet-clipboard-btn">
                 <img
                   src="../src/assets/images/cheatsheet-clipboard-btn.png"
-                  alt="clipboard-button"
+                  alt="clipboard-button" width="15px"
                 />
               </button>
               <p class="cheatsheet-sql">
@@ -81,50 +86,53 @@ async function loadTotalData() {
       })
       .then((data) => {
         // 치트시트 사이드 바 메뉴 클릭 시 스크롤 이벤트 추가
-        const sidebarList = document.querySelectorAll('.list-item li');
-        const sectionList = document.querySelectorAll('.cheatsheet-list');
+        const sidebarList = document.querySelectorAll(".list-item li");
+        const sectionList = document.querySelectorAll(".cheatsheet-list");
 
         sidebarList.forEach((item, i) => {
-          item.addEventListener('click', () => {
+          item.addEventListener("click", () => {
             const sectionTop = sectionList[i]?.offsetTop;
 
             window.scrollTo({
               top: sectionTop - 100,
               left: 0,
-              behavior: 'smooth',
+              behavior: "smooth",
             });
           });
         });
 
         // Cheatsheet Clipboard 복사기능 이벤트 추가
-        const $cheatStatementBtn = document.querySelectorAll(".cheatsheet-clipboard-btn");
+        const $cheatStatementBtn = document.querySelectorAll(
+          ".cheatsheet-clipboard-btn"
+        );
 
         function toast() {
           const $csToastClipBoard = document.querySelector("#toastClipBoard");
 
-          $csToastClipBoard.classList.contains("reveal") ?
-              (clearTimeout(removeToast), removeToast = setTimeout(function () {
-                $csToastClipBoard.classList.remove("reveal")
-              }, 1000)) :
-              removeToast = setTimeout(function () {
-                $csToastClipBoard.classList.remove("reveal")
-              }, 1000)
-              $csToastClipBoard.classList.add("reveal")
+          $csToastClipBoard.classList.contains("reveal")
+            ? (clearTimeout(removeToast),
+              (removeToast = setTimeout(function () {
+                $csToastClipBoard.classList.remove("reveal");
+              }, 1000)))
+            : (removeToast = setTimeout(function () {
+                $csToastClipBoard.classList.remove("reveal");
+              }, 1000));
+          $csToastClipBoard.classList.add("reveal");
         }
 
-        $cheatStatementBtn.forEach(button => {
+        $cheatStatementBtn.forEach((button) => {
           button.addEventListener("click", () => {
-            if(button.nextElementSibling.classList.contains("cheatsheet-sql")){
+            if (
+              button.nextElementSibling.classList.contains("cheatsheet-sql")
+            ) {
               window.navigator.clipboard
-              .writeText(button.nextElementSibling.innerText)
-              .then(() => {
-                toast();
-              });
-            };
+                .writeText(button.nextElementSibling.innerText)
+                .then(() => {
+                  toast();
+                });
+            }
           });
         });
-
-
       });
   }
 }
